b188045213552f3691f97f208ddf89d9cac58f25
eduardomcb <eduardomateuscostab@gmail.com>
2025-01-21 14:31:21 -0300

    Minor changes

--- src/components/ui/know-tech.tsx
+++ src/components/ui/know-tech.tsx
@@ -11,7 +11,7 @@ export default function KnowTech({ tech }: KnownTechProps) {
   return (
     <Card className="h-24 p-4 flex flex-col sm:flex-row items-center">
       <div className="flex-grow text-slate-900 dark:text-slate-200 text-center sm:text-left">
-        <p className="font-medium">{tech.name}</p>
+        <p className="font-medium break-words">{tech.name}</p>
       </div>
       <div className="flex items-center justify-center h-full mt-2 sm:mt-0">
         <CMSIcon icon={tech.iconSvg} />
